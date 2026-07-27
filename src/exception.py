37c29c6070dc6b28f0b75e4d92c9792235162e08
PR1M3-DEV <godswilldavid2468@gmail.com>
2026-07-27 11:13:21 +0100

    Eda and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail: sys):
@@ -17,12 +18,16 @@ def error_message_detail(error, error_detail: sys):
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)
 
         self.error_message = error_message_detail(
             error_message,
-            error_detail=error_detail
+            error_detail
         )
+
+        # Automatically log every exception
+        logging.error(self.error_message)
+
+        super().__init__(self.error_message)
 
     def __str__(self):
         return self.error_message
